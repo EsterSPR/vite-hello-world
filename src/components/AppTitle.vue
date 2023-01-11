67a2fc5c63f273389651e0ba1fb243e5b221c207
EsterSPR <web.esterspreafico@gmail.com>
2023-01-11 15:30:40 +0100

    completato esercizio con bonus

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<!-- ##############SCRIPT############## -->
+<script>
+export default {
+    name: 'AppTitle' //Nome del file, più ordinato
+}
+</script>
+
+<!-- ##############TEMPLATE############## -->
+<template lang="">
+  <div>
+    <h1 id="title">La mia prima app con Vite!</h1>
+    <img src="https://i.kym-cdn.com/entries/icons/mobile/000/038/577/longfacedogheader.jpg" alt="didnidoitfou">
+  </div>
+</template>
+
+<!-- ##############STYLE############## -->
+<style>
+  #title{
+    color: darkcyan;
+    margin-left: 100px;
+    margin-top: 100px;
+  }
+  img{
+    margin-left: 100px;
+    margin-top: 50px;
+  }
+</style>
